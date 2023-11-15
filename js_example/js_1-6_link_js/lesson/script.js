d25e4626a49e82cebcad94702662b2eb5e814266
YaTseChou <arden728003@gmail.com>
2023-11-15 11:49:49 +0800

    Update

--- js_example/js_1-6_link_js/lesson/script.js
+++ js_example/js_1-6_link_js/lesson/script.js
@@ -1,7 +1,23 @@
+window.onload = function () {
+    function changeImg() {
+        document.getElementById('mainPic').src = 'pic02.jpg';
+    }
 
+    function changeImg2() {
+        document.getElementById('mainPic').src = 'pic01.jpg';
+    }
 
+    function changeText() {
+        document.getElementById('main').innerHTML = 'CHANGE!';
+    }
 
+    function changeText2() {
+        document.getElementById('main').innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, odit! Consectetur, voluptatem. Pariatur delectus quas similique fugit facere numquam aut, nobis fugiat, porro cumque rem maiores ab quo, repellat praesentium.';
+    }
 
+    document.getElementById('mainPic').onmouseover = changeImg;
+    document.getElementById('main').onmouseover = changeText;
 
-
-
+    document.getElementById('mainPic').onmouseout = changeImg2;
+    document.getElementById('main').onmouseout = changeText2;
+}
--- js_example/js_1-7_this_var/lesson/script.js
+++ js_example/js_1-7_this_var/lesson/script.js
@@ -1,13 +1,14 @@
+window.onload = function(){
+    function changeImg(){
+        this.src = 'pic02.jpg';
+    }
 
+    function changeImg2(){
+        this.src = 'pic01.jpg';
+    }
 
+    var workImg = document.getElementById('mainPic');
 
-
-
-
-
-
-
-
-
-
-
+    workImg.onmouseover = changeImg;
+    workImg.onmouseout = changeImg2;
+}

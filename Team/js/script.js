7fbab73654d4128e4d6b17c1862d5fe31585418e
YaTseChou <arden728003@gmail.com>
2023-12-11 16:27:39 +0800

    Update

---
+++ Team/js/script.js
@@ -0,0 +1,10 @@
+// $(function(){
+
+//     $('.header_nav_list_left').hide();
+//     $('.header_nav_list_right').hide();
+
+//     $('.Hambar').click(function(){
+//         $('.header_nav_list_left').toggle();
+//         $('.header_nav_list_right').toggle();
+//     })
+// })
